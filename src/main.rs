3a6df92043913ef746f54db731b96b6aba0175d5
Corey Farwell <coreyf@rwell.org>
2017-03-25 13:35:09 -0700

    Update render polygon function to operate on geo polygon.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// extern crate geo;
+extern crate geo;
 extern crate piston;
 extern crate graphics;
 extern crate opengl_graphics;
@@ -8,17 +8,34 @@ mod window;
 
 const RED: graphics::types::Color = [1., 0., 0., 1.];
 
-fn render_polygon(draw_state: &graphics::draw_state::DrawState,
+fn render_polygon(geo_polygon: &geo::Polygon<f64>,
+                  draw_state: &graphics::draw_state::DrawState,
                   transform: graphics::math::Matrix2d,
                   gl: &mut opengl_graphics::GlGraphics) {
-    let polygon = graphics::polygon::Polygon::new(RED);
+    let graphics_polygon = graphics::polygon::Polygon::new(RED);
 
-    let square = [[0., 0.], [10., 0.], [10., 10.], [0., 10.]];
+    let points = geo_polygon.exterior.0
+        .iter()
+        .map(|point| point.0)
+        .map(|coord| [coord.x, coord.y])
+        .collect::<Vec<_>>();
 
-    polygon.draw(&square, draw_state, transform, gl);
+    graphics_polygon.draw(&points, draw_state, transform, gl);
 }
 
 fn main() {
+    let geo_polygon = geo::Polygon {
+        exterior: geo::LineString(
+            vec![
+                geo::Point(geo::Coordinate { x: 0., y: 0. }),
+                geo::Point(geo::Coordinate { x: 10., y: 0. }),
+                geo::Point(geo::Coordinate { x: 10., y: 10. }),
+                geo::Point(geo::Coordinate { x: 0., y: 10. }),
+            ]
+        ),
+        interiors: vec![],
+    };
+
     window::window_loop(|ctx, g| {
         use graphics::{clear, Transformed};
 
@@ -26,6 +43,6 @@ fn main() {
 
         clear([0.0, 0.0, 0.0, 1.0], g);
 
-        render_polygon(&ctx.draw_state, transform, g);
+        render_polygon(&geo_polygon, &ctx.draw_state, transform, g);
    });
 }
