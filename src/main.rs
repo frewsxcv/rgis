d7aec3fce047caf5f51da1655ffd105a956d06d2
Corey Farwell <coreyf@rwell.org>
2017-03-25 13:20:54 -0700

    Move render polygon function back.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,18 @@ extern crate sdl2_window;
 
 mod window;
 
+const RED: graphics::types::Color = [1., 0., 0., 1.];
+
+fn render_polygon(draw_state: &graphics::draw_state::DrawState,
+                  transform: graphics::math::Matrix2d,
+                  gl: &mut opengl_graphics::GlGraphics) {
+    let polygon = graphics::polygon::Polygon::new(RED);
+
+    let square = [[0., 0.], [10., 0.], [10., 10.], [0., 10.]];
+
+    polygon.draw(&square, draw_state, transform, gl);
+}
+
 fn main() {
     window::window_loop(|ctx, g| {
         use graphics::{clear, Transformed};
@@ -14,6 +26,6 @@ fn main() {
 
         clear([0.0, 0.0, 0.0, 1.0], g);
 
-        window::render_polygon(&ctx.draw_state, transform, g);
+        render_polygon(&ctx.draw_state, transform, g);
    });
 }
--- src/window.rs
+++ src/window.rs
@@ -5,20 +5,8 @@ use piston::window::WindowSettings;
 use opengl_graphics::{OpenGL, GlGraphics};
 use sdl2_window::Sdl2Window;
 
-const RED: graphics::types::Color = [1., 0., 0., 1.];
-
 const WINDOW_TITLE: &'static str = "rgis";
 const WINDOW_SIZE: [u32; 2] = [800u32, 800u32];
-
-pub fn render_polygon(draw_state: &graphics::draw_state::DrawState,
-                  transform: graphics::math::Matrix2d,
-                  gl: &mut GlGraphics) {
-    let polygon = graphics::polygon::Polygon::new(RED);
-
-    let square = [[0., 0.], [10., 0.], [10., 10.], [0., 10.]];
-
-    polygon.draw(&square, draw_state, transform, gl);
-}
 
 pub fn window_loop<F>(f: F)
     where F: Fn(graphics::Context, &mut GlGraphics)
