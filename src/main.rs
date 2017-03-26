bf6a1d917532cc4be23ce33e1c95e6d137053f93
Corey Farwell <coreyf@rwell.org>
2017-03-26 10:34:10 -0700

    Operate on geojson file, fix some math.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate geo;
+extern crate geojson;
 extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 extern crate sdl2_window;
+extern crate serde_json;
 
 use geo::boundingbox::BoundingBox;
 use graphics::{clear, Transformed};
@@ -12,7 +14,7 @@ mod window;
 const RED: graphics::types::Color = [1., 0., 0., 1.];
 
 fn render_polygon(geo_polygon: &geo::Polygon<f64>,
-                  draw_state: &graphics::draw_state::DrawState,
+                  draw_state: graphics::draw_state::DrawState,
                   transform: graphics::math::Matrix2d,
                   gl: &mut opengl_graphics::GlGraphics) {
     let graphics_polygon = graphics::polygon::Polygon::new(RED);
@@ -25,33 +27,47 @@ fn render_polygon(geo_polygon: &geo::Polygon<f64>,
     let bbox_height = bbox.ymax - bbox.ymin;
     let y_scale = window::WINDOW_SIZE_Y as f64 / bbox_height;
 
-    let transform = transform.scale(x_scale, y_scale);
+    //let transform = transform.trans(-bbox.xmin, -bbox.ymin);
+    let transform = transform.flip_v();
+    //let transform = transform.scale(x_scale, y_scale);
 
     let points = geo_polygon.exterior.0
         .iter()
         .map(|point| point.0)
+        .map(|coord| geo::Coordinate { x: coord.x - bbox.xmin, y: coord.y - bbox.ymax })
+        .map(|coord| geo::Coordinate { x: coord.x * x_scale, y: coord.y * y_scale })
         .map(|coord| [coord.x, coord.y])
         .collect::<Vec<_>>();
 
-    graphics_polygon.draw(&points, draw_state, transform, gl);
+    graphics_polygon.draw(&points, &draw_state, transform, gl);
 }
 
 fn main() {
-    let geo_polygon = geo::Polygon {
-        exterior: geo::LineString(
-            vec![
-                geo::Point(geo::Coordinate { x: 0., y: 0. }),
-                geo::Point(geo::Coordinate { x: 10., y: 0. }),
-                geo::Point(geo::Coordinate { x: 10., y: 10. }),
-                geo::Point(geo::Coordinate { x: 0., y: 10. }),
-            ]
-        ),
-        interiors: vec![],
+    use std::fs;
+    use serde_json::from_reader;
+    use geojson::conversion::TryInto;
+    let geojson_file = fs::File::open("foo.geojson").unwrap();
+    let geojson_polygon: geojson::GeoJson = from_reader(geojson_file).unwrap();
+    let geojson_polygon = match geojson_polygon {
+        geojson::GeoJson::Geometry(g) => g,
+        _ => unreachable!(),
     };
+    let geo_polygon: geo::Polygon<f64> = geojson_polygon.value.try_into().unwrap();
+
+    // let geo_polygon = geo::Polygon {
+    //     exterior: geo::LineString(
+    //         vec![
+    //             geo::Point(geo::Coordinate { x: 1., y: 1. }),
+    //             geo::Point(geo::Coordinate { x: 3., y: 5. }),
+    //             geo::Point(geo::Coordinate { x: 5., y: 1. }),
+    //         ]
+    //     ),
+    //     interiors: vec![],
+    // };
 
     window::window_loop(|ctx, g| {
         clear([0.0, 0.0, 0.0, 1.0], g);
 
-        render_polygon(&geo_polygon, &ctx.draw_state, ctx.transform, g);
+        render_polygon(&geo_polygon, ctx.draw_state, ctx.transform, g);
    });
 }
