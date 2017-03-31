357bcfc2db3cd3de57eb06e014f9e4baa4149bd9
Corey Farwell <coreyf@rwell.org>
2017-03-30 22:50:52 -0400

    Render LineStrings.

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,37 @@ mod window;
 const RED: graphics::types::Color = [1., 0., 0., 1.];
 const WHITE: graphics::types::Color = [1., 1., 1., 1.];
 
+fn render_line_string(geo_line_string: &geo::LineString<f64>,
+                      draw_state: graphics::draw_state::DrawState,
+                      transform: graphics::math::Matrix2d,
+                      gl: &mut opengl_graphics::GlGraphics) {
+    let graphics_line = graphics::line::Line::new(RED, 1.);
+
+    let bbox = geo_line_string.bbox().unwrap();
+
+    let bbox_width = bbox.xmax - bbox.xmin;
+    let x_scale = window::WINDOW_SIZE_X as f64 / bbox_width;
+
+    let bbox_height = bbox.ymax - bbox.ymin;
+    let y_scale = window::WINDOW_SIZE_Y as f64 / bbox_height;
+
+    //let transform = transform.trans(-bbox.xmin, -bbox.ymin);
+    let transform = transform.flip_v();
+    //let transform = transform.scale(x_scale, y_scale);
+
+    let points = geo_line_string.0
+        .iter()
+        .map(|point| point.0)
+        .map(|coord| geo::Coordinate { x: coord.x - bbox.xmin, y: coord.y - bbox.ymax })
+        .map(|coord| geo::Coordinate { x: coord.x * x_scale, y: coord.y * y_scale })
+        .map(|coord| [coord.x, coord.y])
+        .collect::<Vec<_>>();
+
+    for x in points.windows(2) {
+        graphics_line.draw([x[0][0], x[0][1], x[1][0], x[1][1]], &draw_state, transform, gl);
+    }
+}
+
 fn render_polygon(geo_polygon: &geo::Polygon<f64>,
                   draw_state: graphics::draw_state::DrawState,
                   transform: graphics::math::Matrix2d,
@@ -59,7 +90,18 @@ fn main() {
         geojson::GeoJson::Geometry(g) => g,
         _ => unreachable!(),
     };
-    let geo_polygon: geo::Polygon<f64> = geojson_polygon.value.try_into().unwrap();
+
+    if let Some(geo_polygon) = geojson_polygon.value.clone().try_into().ok() as Option<geo::Polygon<f64>> {
+        window::window_loop(|ctx, g| {
+            clear(WHITE, g);
+            render_polygon(&geo_polygon, ctx.draw_state, ctx.transform, g);
+        });
+    } else if let Some(geo_line_string) = geojson_polygon.value.try_into().ok() as Option<geo::LineString<f64>> {
+        window::window_loop(|ctx, g| {
+            clear(WHITE, g);
+            render_line_string(&geo_line_string, ctx.draw_state, ctx.transform, g);
+        });
+    }
 
     // let geo_polygon = geo::Polygon {
     //     exterior: geo::LineString(
@@ -71,10 +113,4 @@ fn main() {
     //     ),
     //     interiors: vec![],
     // };
-
-    window::window_loop(|ctx, g| {
-        clear(WHITE, g);
-
-        render_polygon(&geo_polygon, ctx.draw_state, ctx.transform, g);
-   });
 }
