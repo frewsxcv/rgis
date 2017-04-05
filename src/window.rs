b21b26ee43a1b662359b7457d3d07685c9141635
Corey Farwell <coreyf@rwell.org>
2017-04-04 23:44:46 -0400

    Refactor out window creation logic.

--- src/window.rs
+++ src/window.rs
@@ -10,17 +10,21 @@ pub const WINDOW_SIZE_X: u32 = 800;
 pub const WINDOW_SIZE_Y: u32 = 800;
 pub const WINDOW_SIZE: [u32; 2] = [WINDOW_SIZE_X, WINDOW_SIZE_Y];
 
+const OPEN_GL_VERSION: OpenGL = OpenGL::V3_2;
+
+fn build_window() -> Sdl2Window {
+    WindowSettings::new(WINDOW_TITLE, WINDOW_SIZE)
+        .exit_on_esc(true)
+        .opengl(OPEN_GL_VERSION)
+        .build()
+        .unwrap()
+}
+
 pub fn window_loop<F>(f: F)
     where F: Fn(graphics::Context, &mut GlGraphics)
 {
-    let opengl = OpenGL::V3_2;
-    let mut window: Sdl2Window = WindowSettings::new(WINDOW_TITLE, WINDOW_SIZE)
-        .exit_on_esc(true)
-        .opengl(opengl)
-        .build()
-        .unwrap();
-
-    let mut gl = GlGraphics::new(opengl);
+    let mut window = build_window();
+    let mut gl = GlGraphics::new(OPEN_GL_VERSION);
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
