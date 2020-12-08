9dfbe9004553db343a88b1643b16f78d9660fec9
Corey Farwell <coreyf@rwell.org>
2020-12-08 12:27:52 -0500

    remove dead code

--- geo-earcutr/benches/big_polygon.rs
+++ geo-earcutr/benches/big_polygon.rs
@@ -12,21 +12,6 @@ fn norway_no_interiors() -> geo_types::Polygon<f64> {
     )
 }
 
-fn norway_with_interiors() -> geo_types::Polygon<f64> {
-    geo_types::Polygon::new(
-        norway_border(),
-        vec![
-            norway_border(),
-            norway_border(),
-            norway_border(),
-            norway_border(),
-            norway_border(),
-            norway_border(),
-            norway_border(),
-        ]
-    )
-}
-
 fn triangulate(polygon: &geo_types::Polygon<f64>) {
     let mut builder = geo_earcutr::Builder::new();
     builder.add_geometry(polygon);
@@ -36,12 +21,6 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_with_input(BenchmarkId::new("Triangulate Norway", "no interiors"), &norway_no_interiors(), |b, norway| {
         b.iter(|| triangulate(norway));
     });
-
-    /*
-    c.bench_with_input(BenchmarkId::new("Triangulate Norway", "with interiors"), &norway_with_interiors(), |b, norway| {
-        b.iter(|| triangulate(norway));
-    });
-    */
 }
 
 criterion_group!(benches, criterion_benchmark);
