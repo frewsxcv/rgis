d82f62dabbe7124f6beffa8d4aaa28445575d4bb
Corey Farwell <coreyf@rwell.org>
2020-12-24 08:13:45 -0500

    fix bug where we wouldnt reproject

--- rgis-cli/src/lib.rs
+++ rgis-cli/src/lib.rs
@@ -58,7 +58,7 @@ pub fn run() -> Values {
             .map(|s| path::PathBuf::from(s.to_owned()))
             .collect(),
         source_srs: matches.value_of("SOURCE SRS").unwrap().to_owned(),
-        target_srs: matches.value_of("SOURCE SRS").unwrap().to_owned(),
+        target_srs: matches.value_of("TARGET SRS").unwrap().to_owned(),
         msaa_sample_count: matches.value_of("MSAA SAMPLE COUNT").unwrap().parse().unwrap(),
     }
 }
