bde86f69b565e17e051236b587466291a2bafa43
Corey Farwell <coreyf@rwell.org>
2021-02-07 16:45:09 -0500

    remove dead code

--- rgis-ui/src/lib.rs
+++ rgis-ui/src/lib.rs
@@ -9,7 +9,6 @@ pub struct PositionText;
 impl Plugin for RgisUi {
     fn build(&self, app: &mut AppBuilder) {
         app
-            // .add_plugin(bevy_megaui::MegaUiPlugin)
             .add_plugin(bevy_egui::EguiPlugin)
             .add_resource(UiState {
                 latitude: 0.,
@@ -17,7 +16,6 @@ impl Plugin for RgisUi {
                 layers: vec![],
             })
             .add_system(ui.system());
-            // .add_system(ui_example.system());
     }
 }
 
@@ -27,20 +25,6 @@ pub struct UiState {
     pub longitude: f32,
     pub layers: Vec<String>,
 }
-
-const WINDOW_MARGIN_X: f32 = 5.;
-const WINDOW_MARGIN_Y: f32 = 5.;
-const WINDOW_WIDTH: f32 = 200.;
-
-const MOUSE_POSITION_WINDOW_OFFSET_X: f32 = WINDOW_MARGIN_X;
-const MOUSE_POSITION_WINDOW_OFFSET_Y: f32 = WINDOW_MARGIN_Y;
-const MOUSE_POSITION_WINDOW_WIDTH: f32 = WINDOW_WIDTH;
-const MOUSE_POSITION_WINDOW_HEIGHT: f32 = 55.;
-
-const LAYERS_WINDOW_OFFSET_X: f32 = WINDOW_MARGIN_X;
-const LAYERS_WINDOW_OFFSET_Y: f32 = MOUSE_POSITION_WINDOW_OFFSET_Y + MOUSE_POSITION_WINDOW_HEIGHT + WINDOW_MARGIN_Y;
-const LAYERS_WINDOW_WIDTH: f32 = WINDOW_WIDTH;
-const LAYERS_WINDOW_HEIGHT: f32 = 200.;
 
 fn ui(mut egui_context: ResMut<bevy_egui::EguiContext>, ui_state: Res<UiState>) {
     let ctx = &mut egui_context.ctx;
