7643fe1229dcd6f7a9c23ac0ce5cf3fd3600855b
Corey Farwell <coreyf@rwell.org>
2021-02-08 11:12:36 -0500

    fix deprecations

--- geo-bevy/src/lib.rs
+++ geo-bevy/src/lib.rs
@@ -1,4 +1,5 @@
 use bevy_render::prelude::*;
+use geo::algorithm::coords_iter::CoordsIter;
 use std::convert::TryFrom;
 
 struct LineStringMeshBuilder {
@@ -16,7 +17,7 @@ impl LineStringMeshBuilder {
     }
 
     /// Call for `add_earcutr_input` for each polygon you want to add to the mesh.
-    fn add_line_string(&mut self, line_string: &geo_types::LineString<f64>) {
+    fn add_line_string(&mut self, line_string: &geo::LineString<f64>) {
         let index_base = self.vertices.len();
         for (i, coord) in line_string.0.iter().enumerate() {
             self.vertices.push([coord.x as f32, coord.y as f32]);
@@ -52,7 +53,7 @@ impl PointMeshBuilder {
     }
 
     /// Call for `add_earcutr_input` for each polygon you want to add to the mesh.
-    fn add_point(&mut self, point: &geo_types::Point<f64>) {
+    fn add_point(&mut self, point: &geo::Point<f64>) {
         let index_base = self.vertices.len();
         self.vertices.push([point.x() as f32, point.y() as f32]);
         self.indices.push(u32::try_from(index_base).unwrap());
@@ -122,26 +123,26 @@ pub trait BuildBevyMeshes {
     fn populate_mesh_builders(&self, ctx: &mut BuildBevyMeshesContext);
 }
 
-impl BuildBevyMeshes for geo_types::Point<f64> {
+impl BuildBevyMeshes for geo::Point<f64> {
     fn populate_mesh_builders(&self, ctx: &mut BuildBevyMeshesContext) {
         ctx.point_mesh_builder.add_point(self);
     }
 }
 
-impl BuildBevyMeshes for geo_types::LineString<f64> {
+impl BuildBevyMeshes for geo::LineString<f64> {
     fn populate_mesh_builders(&self, ctx: &mut BuildBevyMeshesContext) {
         ctx.line_string_mesh_builder.add_line_string(self);
     }
 }
 
-impl BuildBevyMeshes for geo_types::Polygon<f64> {
+impl BuildBevyMeshes for geo::Polygon<f64> {
     fn populate_mesh_builders(&self, ctx: &mut BuildBevyMeshesContext) {
         ctx.polygon_mesh_builder
             .add_earcutr_input(polygon_to_earcutr_input(self));
     }
 }
 
-impl BuildBevyMeshes for geo_types::MultiPoint<f64> {
+impl BuildBevyMeshes for geo::MultiPoint<f64> {
     fn populate_mesh_builders(&self, ctx: &mut BuildBevyMeshesContext) {
         for point in &self.0 {
             point.populate_mesh_builders(ctx);
@@ -149,7 +150,7 @@ impl BuildBevyMeshes for geo_types::MultiPoint<f64> {
     }
 }
 
-impl BuildBevyMeshes for geo_types::MultiLineString<f64> {
+impl BuildBevyMeshes for geo::MultiLineString<f64> {
     fn populate_mesh_builders(&self, ctx: &mut BuildBevyMeshesContext) {
         for line_string in &self.0 {
             line_string.populate_mesh_builders(ctx);
@@ -157,7 +158,7 @@ impl BuildBevyMeshes for geo_types::MultiLineString<f64> {
     }
 }
 
-impl BuildBevyMeshes for geo_types::MultiPolygon<f64> {
+impl BuildBevyMeshes for geo::MultiPolygon<f64> {
     fn populate_mesh_builders(&self, ctx: &mut BuildBevyMeshesContext) {
         for polygon in &self.0 {
             polygon.populate_mesh_builders(ctx);
@@ -165,22 +166,22 @@ impl BuildBevyMeshes for geo_types::MultiPolygon<f64> {
     }
 }
 
-impl BuildBevyMeshes for geo_types::Geometry<f64> {
+impl BuildBevyMeshes for geo::Geometry<f64> {
     fn populate_mesh_builders(&self, ctx: &mut BuildBevyMeshesContext) {
         match self {
-            geo_types::Geometry::Point(g) => g.populate_mesh_builders(ctx),
-            geo_types::Geometry::LineString(g) => g.populate_mesh_builders(ctx),
-            geo_types::Geometry::Polygon(g) => g.populate_mesh_builders(ctx),
-            geo_types::Geometry::MultiPoint(g) => g.populate_mesh_builders(ctx),
-            geo_types::Geometry::MultiLineString(g) => g.populate_mesh_builders(ctx),
-            geo_types::Geometry::MultiPolygon(g) => g.populate_mesh_builders(ctx),
-            geo_types::Geometry::GeometryCollection(g) => g.populate_mesh_builders(ctx),
+            geo::Geometry::Point(g) => g.populate_mesh_builders(ctx),
+            geo::Geometry::LineString(g) => g.populate_mesh_builders(ctx),
+            geo::Geometry::Polygon(g) => g.populate_mesh_builders(ctx),
+            geo::Geometry::MultiPoint(g) => g.populate_mesh_builders(ctx),
+            geo::Geometry::MultiLineString(g) => g.populate_mesh_builders(ctx),
+            geo::Geometry::MultiPolygon(g) => g.populate_mesh_builders(ctx),
+            geo::Geometry::GeometryCollection(g) => g.populate_mesh_builders(ctx),
             _ => bevy_log::error!("Encountered unrenderable geometry type"),
         }
     }
 }
 
-impl BuildBevyMeshes for geo_types::GeometryCollection<f64> {
+impl BuildBevyMeshes for geo::GeometryCollection<f64> {
     fn populate_mesh_builders(&self, ctx: &mut BuildBevyMeshesContext) {
         for g in self {
             g.populate_mesh_builders(ctx);
@@ -188,8 +189,8 @@ impl BuildBevyMeshes for geo_types::GeometryCollection<f64> {
     }
 }
 
-fn polygon_to_earcutr_input(polygon: &geo_types::Polygon<f64>) -> bevy_earcutr::EarcutrInput {
-    let mut vertices = Vec::with_capacity(polygon_num_coords(polygon) * 2);
+fn polygon_to_earcutr_input(polygon: &geo::Polygon<f64>) -> bevy_earcutr::EarcutrInput {
+    let mut vertices = Vec::with_capacity(polygon.coords_count() * 2);
     let mut interior_indices = Vec::with_capacity(polygon.interiors().len());
 
     flat_line_string_coords_2(polygon.exterior(), &mut vertices);
@@ -205,16 +206,7 @@ fn polygon_to_earcutr_input(polygon: &geo_types::Polygon<f64>) -> bevy_earcutr::
     }
 }
 
-fn polygon_num_coords(polygon: &geo_types::Polygon<f64>) -> usize {
-    polygon.exterior().num_coords()
-        + polygon
-            .interiors()
-            .iter()
-            .map(geo_types::LineString::num_coords)
-            .sum::<usize>()
-}
-
-fn flat_line_string_coords_2(line_string: &geo_types::LineString<f64>, vertices: &mut Vec<f64>) {
+fn flat_line_string_coords_2(line_string: &geo::LineString<f64>, vertices: &mut Vec<f64>) {
     for coord in &line_string.0 {
         vertices.push(coord.x);
         vertices.push(coord.y);
