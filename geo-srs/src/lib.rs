c09f23259cfa07fb6cc93a6f0821f08df717e667
Corey Farwell <coreyf@rwell.org>
2022-01-29 12:15:47 -0500

    add a transform trait

--- geo-srs/src/lib.rs
+++ geo-srs/src/lib.rs
@@ -1,5 +1,5 @@
 use geo::algorithm::contains::Contains;
-use geo::algorithm::map_coords::MapCoordsInplace;
+use std::{error::Error, fmt};
 
 #[derive(Debug, Clone)]
 pub struct CoordWithSrs<T: geo::CoordFloat> {
@@ -62,12 +62,60 @@ impl<T: geo::CoordFloat> GeometryWithSrs<T> {
     // }
 
     pub fn reproject(&mut self, target_srs: &str) {
-        debug_assert!(!target_srs.is_empty());
-
-        let projector = proj::Proj::new_known_crs(&self.srs, target_srs, None).unwrap();
-
-        self.geometry
-            .map_coords_inplace(|&(x, y)| projector.convert((x, y)).unwrap());
+        self.geometry = self.geometry.transform(&self.srs, target_srs).unwrap();
         self.srs = target_srs.to_owned();
+    }
+}
+
+/// Transform a Geometry from one CRS to another.
+///
+/// # Examples
+///
+/// ```
+/// ```
+trait Transform<T> {
+    type Output;
+
+    fn transform(&self, source_srs: &str, target: &str) -> Result<Self::Output, TransformError>;
+}
+
+#[derive(Debug)]
+enum TransformError {
+    UnknownCrs,
+    ProjError(Box<dyn Error + Send + Sync>),
+}
+
+impl fmt::Display for TransformError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TransformError::UnknownCrs => write!(f, "Unknown CRS"),
+            TransformError::ProjError(err) => write!(f, "{}", err),
+        }
+    }
+}
+
+impl Error for TransformError {}
+
+impl<T, G> Transform<T> for G
+where
+    T: geo::CoordFloat,
+    G: geo::algorithm::map_coords::TryMapCoords<T, T>,
+{
+    type Output = G::Output;
+
+    fn transform(
+        &self,
+        source_srs: &str,
+        target_srs: &str,
+    ) -> Result<Self::Output, TransformError> {
+        let transformer = proj::Proj::new_known_crs(source_srs, target_srs, None)
+            .ok_or(TransformError::UnknownCrs)?;
+
+        self.try_map_coords(|&(x, y)| {
+            transformer
+                .convert((x, y))
+                .map_err(|e| Box::new(e) as Box<dyn Error + Send + Sync>)
+        })
+        .map_err(|e| TransformError::ProjError(e))
     }
 }
