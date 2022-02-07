c3a0b34fd2eb78041fd84bdd47baa186ed05f3ee
Corey Farwell <coreyf@rwell.org>
2022-02-07 09:44:25 -0500

    add rgis renderer crate

--- rgis/src/main.rs
+++ rgis/src/main.rs
@@ -1,89 +1,4 @@
-use bevy::{app::Events, core_pipeline::ClearColor, prelude::*};
-use geo_bevy::BuildBevyMeshes;
-
-// System
-fn layer_loaded(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    layers: rgis_layers::ResLayers,
-    mut event_reader: EventReader<rgis_layers::LayerLoaded>,
-    mut spawned_events: ResMut<Events<rgis_layers::LayerSpawned>>,
-) {
-    for event in event_reader.iter() {
-        let layers = layers.read().unwrap();
-        let layer = match layers.get(event.0) {
-            Some(l) => l,
-            None => continue,
-        };
-
-        if !layer.visible {
-            continue;
-        }
-
-        // TODO: store the result of `add` here, so it can be removed later
-        let material = materials.add(layer.color.into());
-
-        let tl = time_logger::start(&format!("Triangulating and building {} mesh", layer.name));
-        for mesh in layer
-            .projected_geometry
-            .geometry
-            .build_bevy_meshes(geo_bevy::BuildBevyMeshesContext::new())
-        {
-            spawn_mesh(mesh, material.clone(), &mut meshes, &mut commands);
-        }
-        tl.finish();
-
-        spawned_events.send(rgis_layers::LayerSpawned(event.0));
-    }
-}
-
-struct LayerSpawnedPlugin;
-
-impl Plugin for LayerSpawnedPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_system(layer_spawned.system());
-    }
-}
-
-// System
-fn layer_spawned(
-    layers: rgis_layers::ResLayers,
-    mut camera_offset: ResMut<rgis_camera::CameraOffset>,
-    mut camera_scale: ResMut<rgis_camera::CameraScale>,
-    mut event_reader: EventReader<rgis_layers::LayerSpawned>,
-) {
-    for event in event_reader.iter() {
-        let layers = layers.read().unwrap();
-        let layer = match layers.get(event.0) {
-            Some(l) => l,
-            None => continue,
-        };
-        let layer_center = layer.projected_bounding_rect.rect.center();
-        // TODO: this scale math is inprecise. it should take into account
-        // .     the height of the geometry. as well as the window size.
-        let scale = layer.projected_bounding_rect.rect.width() / 1_000.;
-        // TODO: only change the transform if there were no layers previously
-        debug!("Moving camera to look at new layer");
-        camera_offset.x = layer_center.x as f32;
-        camera_offset.y = layer_center.y as f32;
-        camera_scale.0 = scale as f32;
-    }
-}
-
-pub fn spawn_mesh(
-    mesh: Mesh,
-    material: Handle<ColorMaterial>,
-    meshes: &mut Assets<Mesh>,
-    commands: &mut Commands,
-) {
-    let mmb = bevy::sprite::MaterialMesh2dBundle {
-        material,
-        mesh: bevy::sprite::Mesh2dHandle(meshes.add(mesh)),
-        ..Default::default()
-    };
-    commands.spawn_bundle(mmb);
-}
+use bevy::{core_pipeline::ClearColor, prelude::*};
 
 fn main() {
     let cli_values = rgis_cli::run();
@@ -122,10 +37,9 @@ fn main() {
         .add_plugin(rgis_cli::Plugin(cli_values))
         .add_plugin(rgis_layers::RgisLayersPlugin)
         .add_plugin(rgis_file_loader::RgisFileLoaderPlugin)
-        .add_system(layer_loaded.system())
+        .add_plugin(rgis_renderer::RgisRendererPlugin)
         .add_plugin(rgis_mouse::Plugin)
         .add_plugin(rgis_keyboard::Plugin)
-        .add_plugin(LayerSpawnedPlugin)
         .add_plugin(rgis_camera::RgisCamera)
         .add_plugin(rgis_ui::RgisUi {
             source_srs,
