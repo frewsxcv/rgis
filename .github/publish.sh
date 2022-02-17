2aa7f9b91f0c68bdc5bd03a5bddf43049b0e15d6
Corey Farwell <coreyf@rwell.org>
2022-02-17 16:15:53 -0700

    git author

--- .github/publish.sh
+++ .github/publish.sh
@@ -7,6 +7,8 @@ wasm-pack build rgis
 (cd www && npm run build)
 cp -R www/dist/* .
 git add -A
+git config user.name "Automated"
+git config user.email "actions@users.noreply.github.com"
 git commit -m 'publish'
 git push -f origin gh-pages
 
