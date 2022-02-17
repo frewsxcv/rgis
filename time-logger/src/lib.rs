1c5f5a73e04322f24f33efcaf3d8d973b75f3bd0
Corey Farwell <coreyf@rwell.org>
2022-02-17 08:23:30 -0700

    make time-logger compatible with wasm

--- time-logger/src/lib.rs
+++ time-logger/src/lib.rs
@@ -1,22 +1,40 @@
 use std::time;
 
+#[cfg(target_arch = "wasm32")]
+type Instant = ();
+#[cfg(not(target_arch = "wasm32"))]
+type Instant = time::Instant;
+
 pub struct TimeLogger {
     title: String,
-    duration: time::Instant,
+    duration: Instant,
+}
+
+#[cfg(target_arch = "wasm32")]
+fn new_instant() -> Instant {
+    ()
+}
+#[cfg(not(target_arch = "wasm32"))]
+fn new_instant() -> Instant {
+    time::Instant::now()
 }
 
 impl TimeLogger {
     pub fn start(title: impl Into<String>) -> Self {
         let title: String = title.into();
         bevy_log::info!("{}: started", title);
+
         TimeLogger {
             title,
-            duration: time::Instant::now(),
+            duration: new_instant()
         }
     }
 
     pub fn finish(self) {
-        bevy_log::info!("{}: done ({:?})", self.title, self.duration.elapsed());
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            bevy_log::info!("{}: done ({:?})", self.title, self.duration.elapsed());
+        }
     }
 }
 
