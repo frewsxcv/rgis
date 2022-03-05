8566bae810273b5cb5a7cda819f0811b00d1ae4b
Corey Farwell <coreyf@rwell.org>
2022-03-05 10:29:41 -0500

    reduce size of layer id

--- rgis-layer-id/src/lib.rs
+++ rgis-layer-id/src/lib.rs
@@ -1,4 +1,4 @@
 #[derive(
     Copy, Clone, Debug, Eq, Ord, PartialEq, PartialOrd, Hash, bevy::ecs::component::Component,
 )]
-pub struct LayerId(pub i64); // todo: we can make this an i16
+pub struct LayerId(pub u16);
