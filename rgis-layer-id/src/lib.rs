845a4542ab80ecae44b5e84903bad9134b30cc41
Corey Farwell <coreyf@rwell.org>
2022-03-05 10:28:48 -0500

    take advantage of bevy’s ecs system

--- rgis-layer-id/src/lib.rs
+++ rgis-layer-id/src/lib.rs
@@ -1,2 +1,4 @@
-#[derive(Copy, Clone, Debug, Eq, Ord, PartialEq, PartialOrd, Hash)]
-pub struct LayerId(pub i64);
+#[derive(
+    Copy, Clone, Debug, Eq, Ord, PartialEq, PartialOrd, Hash, bevy::ecs::component::Component,
+)]
+pub struct LayerId(pub i64); // todo: we can make this an i16
--- rgis-renderer/src/lib.rs
+++ rgis-renderer/src/lib.rs
@@ -1,10 +1,5 @@
 use bevy::{app::Events, prelude::*};
 use geo_bevy::BuildBevyMeshes;
-use std::collections;
-
-// Change this to a query?
-#[derive(Default)]
-struct EntityStore(collections::HashMap<rgis_layer_id::LayerId, Vec<bevy::ecs::entity::Entity>>);
 
 // System
 fn layer_loaded(
@@ -14,7 +9,6 @@ fn layer_loaded(
     layers: Res<rgis_layers::ArcLayers>,
     mut event_reader: EventReader<rgis_events::LayerLoadedEvent>,
     mut center_camera_events: ResMut<Events<rgis_events::CenterCameraEvent>>,
-    mut entity_store: ResMut<EntityStore>,
 ) {
     for event in event_reader.iter() {
         let layers = layers.read().unwrap();
@@ -32,7 +26,6 @@ fn layer_loaded(
             &layer,
             &mut commands,
             &mut meshes,
-            &mut entity_store,
             layer.color,
         );
         center_camera_events.send(rgis_events::CenterCameraEvent(layer.id));
@@ -45,8 +38,7 @@ impl Plugin for RgisRendererPlugin {
     fn build(&self, app: &mut App) {
         app.add_system(layer_loaded)
             .add_system(toggle_material_event)
-            .add_system(handle_layer_color_changed_event)
-            .insert_resource(EntityStore::default());
+            .add_system(handle_layer_color_changed_event);
     }
 }
 
@@ -55,7 +47,6 @@ fn spawn_geometry_mesh(
     layer: &rgis_layers::Layer,
     commands: &mut Commands,
     meshes: &mut Assets<Mesh>,
-    entity_store: &mut EntityStore,
     color: bevy::prelude::Color,
 ) {
     let material = materials.add(color.into());
@@ -65,14 +56,7 @@ fn spawn_geometry_mesh(
         .projected_geometry
         .build_bevy_meshes(geo_bevy::BuildBevyMeshesContext::new())
     {
-        spawn_mesh(
-            mesh,
-            material.clone(),
-            meshes,
-            commands,
-            entity_store,
-            layer.id,
-        );
+        spawn_mesh(mesh, material.clone(), meshes, commands, layer.id);
     }
     tl.finish();
 }
@@ -80,10 +64,10 @@ fn spawn_geometry_mesh(
 fn toggle_material_event(
     layers: Res<rgis_layers::ArcLayers>,
     mut event_reader: EventReader<rgis_events::ToggleMaterialEvent>,
-    mut entity_store: ResMut<EntityStore>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    query: Query<(&rgis_layer_id::LayerId, &Handle<ColorMaterial>, Entity)>,
 ) {
     for event in event_reader.iter() {
         let layers = layers.read().unwrap();
@@ -99,23 +83,16 @@ fn toggle_material_event(
                     layer,
                     &mut commands,
                     &mut meshes,
-                    &mut entity_store,
                     layer.color,
                 );
             }
-            rgis_events::ToggleMaterialEvent::Hide(layer_id) => {
-                let layer = match layers.get(*layer_id) {
-                    Some(l) => l,
-                    None => continue,
-                };
-
-                let entities = match entity_store.0.remove(&layer.id) {
-                    Some(h) => h,
-                    None => continue,
-                };
-                for entity in entities {
-                    let mut entity_commands = commands.entity(entity);
-                    entity_commands.despawn();
+            rgis_events::ToggleMaterialEvent::Hide(event_layer_id) => {
+                // TODO: remove mesh from asset store?
+                for (layer_id, _color_material, entity) in query.iter() {
+                    if layer_id == event_layer_id {
+                        let mut entity_commands = commands.entity(entity);
+                        entity_commands.despawn();
+                    }
                 }
             }
         }
@@ -125,8 +102,7 @@ fn toggle_material_event(
 fn handle_layer_color_changed_event(
     mut events: EventReader<rgis_events::LayerColorUpdated>,
     layers: Res<rgis_layers::ArcLayers>,
-    entity_store: Res<EntityStore>,
-    query: Query<(Entity, &Handle<ColorMaterial>)>,
+    query: Query<(&rgis_layer_id::LayerId, &Handle<ColorMaterial>)>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     for event in events.iter() {
@@ -136,13 +112,8 @@ fn handle_layer_color_changed_event(
             None => continue,
         };
 
-        let entities = match entity_store.0.get(&layer.id) {
-            Some(h) => h,
-            None => continue,
-        };
-
-        for handle in query.iter().filter_map(|(entity, handle)| {
-            if entities.contains(&entity) {
+        for handle in query.iter().filter_map(|(layer_id, handle)| {
+            if *layer_id == event.0 {
                 Some(handle)
             } else {
                 None
@@ -158,7 +129,6 @@ fn spawn_mesh(
     material: Handle<ColorMaterial>,
     meshes: &mut Assets<Mesh>,
     commands: &mut Commands,
-    entity_store: &mut EntityStore,
     layer_id: rgis_layer_id::LayerId,
 ) {
     let mmb = bevy::sprite::MaterialMesh2dBundle {
@@ -166,10 +136,5 @@ fn spawn_mesh(
         mesh: bevy::sprite::Mesh2dHandle(meshes.add(mesh)),
         ..Default::default()
     };
-    let entity_commands = commands.spawn_bundle(mmb);
-    entity_store
-        .0
-        .entry(layer_id)
-        .or_default()
-        .push(entity_commands.id());
+    commands.spawn_bundle(mmb).insert(layer_id);
 }
