11335bbb9923237bb2b3e83993949c8af108b92b
Corey Farwell <coreyf@rwell.org>
2022-03-14 10:42:05 -0400

    bump bevy-web-resizer

--- rgis/src/lib.rs
+++ rgis/src/lib.rs
@@ -39,7 +39,7 @@ pub fn run() {
 
     #[cfg(target_arch = "wasm32")]
     {
-        app.add_system(bevy_web_resizer::web_resize_system);
+        app.add_plugin(bevy_web_resizer::Plugin);
     }
 
     app.run();
