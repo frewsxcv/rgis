cbab7f6f5f00489c0f138f815318ebe029f4c8ab
Corey Farwell <coreyf@rwell.org>
2022-03-27 21:55:24 -0400

    add library entry for countries

--- rgis-library/src/lib.rs
+++ rgis-library/src/lib.rs
@@ -4,12 +4,16 @@ pub struct LibraryEntry {
     pub crs: &'static str,
 }
 
-pub const ENTRIES: [LibraryEntry; 2] = [LibraryEntry {
+pub const ENTRIES: [LibraryEntry; 3] = [LibraryEntry {
     name: "USA States",
     url: "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json",
     crs: "EPSG:4326",
 }, LibraryEntry {
     name: "Tectonic Plate Boundaries",
     url: "https://raw.githubusercontent.com/andrea-ballatore/open-geo-data-education/main/datasets/tectonic_plates_2002/PB2002_boundaries.geojson",
+    crs: "EPSG:4326",
+}, LibraryEntry {
+    name: "Countries",
+    url: "https://raw.githubusercontent.com/andrea-ballatore/open-geo-data-education/main/datasets/world_country_boundaries_2018/natural_earth_world_boundaries_50m_2018.geojson",
     crs: "EPSG:4326",
 }];
