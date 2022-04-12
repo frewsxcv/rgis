67d19ae3e3b1b6242cd2ea7a462e840b83605e5a
Corey Farwell <coreyf@rwell.org>
2022-04-12 10:39:14 -0400

    add amtrak entries to library

--- rgis-library/src/lib.rs
+++ rgis-library/src/lib.rs
@@ -11,7 +11,7 @@ pub struct LibraryEntry {
     pub crs: &'static str,
 }
 
-pub const ENTRIES: [LibraryEntry; 3] = [LibraryEntry {
+pub const ENTRIES: [LibraryEntry; 5] = [LibraryEntry {
     name: "USA States",
     url: "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json",
     crs: "EPSG:4326",
@@ -22,5 +22,13 @@ pub const ENTRIES: [LibraryEntry; 3] = [LibraryEntry {
 }, LibraryEntry {
     name: "Countries",
     url: "https://raw.githubusercontent.com/andrea-ballatore/open-geo-data-education/main/datasets/world_country_boundaries_2018/natural_earth_world_boundaries_50m_2018.geojson",
+    crs: "EPSG:4326",
+}, LibraryEntry {
+    name: "Amtrak Stations",
+    url: "https://raw.githubusercontent.com/datanews/amtrak-geojson/master/amtrak-stations.geojson",
+    crs: "EPSG:4326",
+}, LibraryEntry {
+    name: "Amtrak Tracks",
+    url: "https://raw.githubusercontent.com/datanews/amtrak-geojson/master/amtrak-track.geojson",
     crs: "EPSG:4326",
 }];
