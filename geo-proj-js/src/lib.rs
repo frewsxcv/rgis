747a8a7fe910fbfac48944631a2fe8a0732b1aac
Corey Farwell <coreyf@rwell.org>
2022-06-03 22:40:43 -0400

    fix compilation errors after cargo update

--- geo-proj-js/src/lib.rs
+++ geo-proj-js/src/lib.rs
@@ -39,16 +39,20 @@ pub fn transform(
         )
         .map_err(|_| Error)?;
     let array = js_sys::Array::new_with_length(2);
-    let forward = js_sys::Reflect::get(&projector, &"forward".into())
-        .map_err(|_| Error)?
-        .dyn_into::<js_sys::Function>()
-        .map_err(|_| Error)?;
+    let forward = unsafe {
+        js_sys::Reflect::get(&projector, &"forward".into())
+            .map_err(|_| Error)?
+            .dyn_into::<js_sys::Function>()
+            .map_err(|_| Error)?
+    };
 
-    geometry.map_coords_in_place(|(x, y)| match in_place((x, y), &forward, &array) {
-        Ok(n) => n,
-        Err(e) => {
-            log::error!("Failed to convert coordinate: {:?}", e);
-            (x, y)
+    geometry.map_coords_in_place(|geo::Coordinate { x, y }| {
+        match in_place((x, y), &forward, &array) {
+            Ok(n) => n,
+            Err(e) => {
+                log::error!("Failed to convert coordinate: {:?}", e);
+                geo::Coordinate { x, y }
+            }
         }
     });
 
@@ -61,7 +65,7 @@ fn in_place(
     (x, y): (f64, f64),
     forward: &js_sys::Function,
     array: &js_sys::Array,
-) -> Result<(f64, f64), Box<dyn error::Error + Send + Sync>> {
+) -> Result<geo::Coordinate<f64>, Error> {
     array.set(0, wasm_bindgen::JsValue::from_f64(x));
     array.set(1, wasm_bindgen::JsValue::from_f64(y));
     let result = forward
@@ -69,8 +73,8 @@ fn in_place(
         .map_err(|_| Error)?
         .dyn_into::<js_sys::Array>()
         .map_err(|_| Error)?;
-    Ok((
-        result.get(0).as_f64().ok_or(Error)?,
-        result.get(1).as_f64().ok_or(Error)?,
-    ))
+    Ok(geo::Coordinate {
+        x: result.get(0).as_f64().ok_or(Error)?,
+        y: result.get(1).as_f64().ok_or(Error)?,
+    })
 }
