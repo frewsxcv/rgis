fc50355bbd24089c60af1d3ebbab0c3de8890a86
Corey Farwell <coreyf@rwell.org>
2022-06-22 23:06:41 -0400

    set proj log level to disable console logging. fixes #71

--- rgis-transform/src/lib.rs
+++ rgis-transform/src/lib.rs
@@ -12,6 +12,13 @@ pub struct Plugin;
 
 impl bevy::app::Plugin for Plugin {
     fn build(&self, app: &mut bevy::app::App) {
-        app.add_system_set(systems::system_set());
+        app.add_system_set(systems::system_set())
+            .add_startup_system(set_proj_log_level);
+    }
+}
+
+fn set_proj_log_level() {
+    unsafe {
+        proj_sys::proj_log_level(std::ptr::null_mut(), proj_sys::PJ_LOG_LEVEL_PJ_LOG_NONE);
     }
 }
