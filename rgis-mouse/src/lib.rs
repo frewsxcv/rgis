e41b4c762851b8fc6d9ec7f0b66c2c2856488d9e
Corey Farwell <coreyf@rwell.org>
2022-07-14 22:40:14 -0400

    split mouse systems into click and move

--- rgis-mouse/src/lib.rs
+++ rgis-mouse/src/lib.rs
@@ -56,16 +56,13 @@ fn screen_coords_to_geo_coords(
     }
 }
 
-#[allow(clippy::too_many_arguments)]
 fn mouse_motion_system(
     mut mouse_motion_event_reader: bevy::ecs::event::EventReader<bevy::input::mouse::MouseMotion>,
-    mut map_clicked_event_writer: bevy::ecs::event::EventWriter<rgis_events::MapClickedEvent>,
     mouse_button: Res<bevy::input::Input<bevy::input::mouse::MouseButton>>,
     mut pan_camera_events: bevy::ecs::event::EventWriter<rgis_events::PanCameraEvent>,
     mut windows: ResMut<bevy::window::Windows>,
     mut bevy_egui_ctx: ResMut<bevy_egui::EguiContext>,
     rgis_settings: Res<rgis_settings::RgisSettings>,
-    mouse_position: Res<MousePos>,
 ) {
     if bevy_egui_ctx.ctx_mut().is_pointer_over_area() {
         windows
@@ -74,39 +71,30 @@ fn mouse_motion_system(
         return;
     }
 
-    match rgis_settings.current_tool {
-        rgis_settings::Tool::Pan => {
-            if mouse_button.pressed(bevy::input::mouse::MouseButton::Left)
-                || mouse_button.pressed(bevy::input::mouse::MouseButton::Right)
-            {
-                windows
-                    .primary_mut()
-                    .set_cursor_icon(bevy::window::CursorIcon::Grabbing);
-                let mut x_sum = 0.;
-                let mut y_sum = 0.;
-                for event in mouse_motion_event_reader.iter() {
-                    // If the mouse is dragging rightward, `delta.x` will be positive. In this case, we
-                    // want the map to move right, and the camera to move left. We need to negate the
-                    // delta X value.
-                    x_sum -= event.delta.x;
+    if rgis_settings.current_tool == rgis_settings::Tool::Pan
+        && mouse_button.pressed(bevy::input::mouse::MouseButton::Left)
+        || mouse_button.pressed(bevy::input::mouse::MouseButton::Right)
+    {
+        windows
+            .primary_mut()
+            .set_cursor_icon(bevy::window::CursorIcon::Grabbing);
+        let mut x_sum = 0.;
+        let mut y_sum = 0.;
+        for event in mouse_motion_event_reader.iter() {
+            // If the mouse is dragging rightward, `delta.x` will be positive. In this case, we
+            // want the map to move right, and the camera to move left. We need to negate the
+            // delta X value.
+            x_sum -= event.delta.x;
 
-                    // If the mouse is dragging upward, `delta.y` will be negative. In this case, we
-                    // want the map to move up, and the camera to move down. We do not need to negate
-                    // the delta Y value.
-                    y_sum += event.delta.y;
-                }
-                if x_sum != 0. || y_sum != 0. {
-                    pan_camera_events.send(rgis_events::PanCameraEvent { x: x_sum, y: y_sum });
-                }
-                return;
-            }
+            // If the mouse is dragging upward, `delta.y` will be negative. In this case, we
+            // want the map to move up, and the camera to move down. We do not need to negate
+            // the delta Y value.
+            y_sum += event.delta.y;
         }
-        rgis_settings::Tool::Query => {
-            if mouse_button.just_pressed(bevy::input::mouse::MouseButton::Left) {
-                map_clicked_event_writer
-                    .send(rgis_events::MapClickedEvent(mouse_position.projected))
-            }
+        if x_sum != 0. || y_sum != 0. {
+            pan_camera_events.send(rgis_events::PanCameraEvent { x: x_sum, y: y_sum });
         }
+        return;
     }
 
     let cursor_icon = match rgis_settings.current_tool {
@@ -114,6 +102,19 @@ fn mouse_motion_system(
         rgis_settings::Tool::Query => bevy::window::CursorIcon::Crosshair,
     };
     windows.primary_mut().set_cursor_icon(cursor_icon);
+}
+
+fn mouse_click_system(
+    mut map_clicked_event_writer: bevy::ecs::event::EventWriter<rgis_events::MapClickedEvent>,
+    mouse_button: Res<bevy::input::Input<bevy::input::mouse::MouseButton>>,
+    rgis_settings: Res<rgis_settings::RgisSettings>,
+    mouse_position: Res<MousePos>,
+) {
+    if rgis_settings.current_tool == rgis_settings::Tool::Query
+        && mouse_button.just_pressed(bevy::input::mouse::MouseButton::Left)
+    {
+        map_clicked_event_writer.send(rgis_events::MapClickedEvent(mouse_position.projected))
+    }
 }
 
 fn mouse_scroll_system(
@@ -143,6 +144,7 @@ impl bevy::app::Plugin for Plugin {
     fn build(&self, app: &mut bevy::app::App) {
         app.add_system(cursor_moved_system)
             .add_system(mouse_scroll_system)
+            .add_system(mouse_click_system)
             .add_system(mouse_motion_system)
             .insert_resource(MousePos {
                 projected: geo::Coordinate { x: 0., y: 0. },
