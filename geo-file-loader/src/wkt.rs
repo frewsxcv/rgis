25ae0d7f9db420e6d858ecb571e9262ae3f58d9b
Corey Farwell <coreyf@rwell.org>
2022-09-02 17:44:25 -0400

    add an (unused) wkt reader

--- geo-file-loader/src/wkt.rs
+++ geo-file-loader/src/wkt.rs
@@ -1,15 +1,22 @@
-/*
-struct WktSource;
+use std::io;
+
+use geozero::GeozeroDatasource;
+
+struct WktSource {
+    bytes: Vec<u8>,
+}
 
 impl crate::FileLoader for WktSource {
-    type Error = LoadGeoJsonError;
+    type Error = ();
+
+    const FILE_TYPE_NAME: &'static str = "WKT";
 
     fn load(self) -> Result<geo_features::FeatureCollection, Self::Error> {
-        Ok(match self {
-            #[cfg(not(target_arch = "wasm32"))]
-            GeoJsonSource::Path(path) => load_from_path(&path)?,
-            GeoJsonSource::Bytes(bytes) => load_from_reader(io::Cursor::new(bytes))?,
-        })
+        let mut bytes_cursor = io::Cursor::new(&self.bytes);
+        let mut wkt_reader = geozero::wkt::WktReader(&mut bytes_cursor);
+        let mut geo_writer = geozero::geo_types::GeoWriter::new();
+        wkt_reader.process(&mut geo_writer).unwrap();
+        let geometry = geo_writer.take_geometry().unwrap();
+        Ok(geo_features::FeatureCollection::from_geometry(geometry).unwrap())
     }
 }
-*/
