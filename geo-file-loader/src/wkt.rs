c8d18718a9e19ecc2fab418dc904ec4d3bd545f2
Corey Farwell <coreyf@rwell.org>
2022-09-02 22:43:29 -0400

    one fewer unwrap

--- geo-file-loader/src/wkt.rs
+++ geo-file-loader/src/wkt.rs
@@ -7,7 +7,7 @@ pub struct WktSource {
 }
 
 impl crate::FileLoader for WktSource {
-    type Error = ();
+    type Error = geozero::error::GeozeroError;
 
     const FILE_TYPE_NAME: &'static str = "WKT";
 
@@ -19,7 +19,7 @@ impl crate::FileLoader for WktSource {
         let mut bytes_cursor = io::Cursor::new(&self.bytes);
         let mut wkt_reader = geozero::wkt::WktReader(&mut bytes_cursor);
         let mut geo_writer = geozero::geo_types::GeoWriter::new();
-        wkt_reader.process(&mut geo_writer).unwrap();
+        wkt_reader.process(&mut geo_writer)?;
         let geometry = geo_writer.take_geometry().unwrap();
         Ok(geo_features::FeatureCollection::from_geometry(geometry).unwrap())
     }
