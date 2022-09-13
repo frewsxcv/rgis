8f484e36b682db075b426b261cc4f423c102959a
Corey Farwell <coreyf@rwell.org>
2022-09-13 00:53:16 -0400

    fix #84. delete sprite entities upon layer removal

--- rgis-renderer/src/systems.rs
+++ rgis-renderer/src/systems.rs
@@ -96,7 +96,10 @@ fn handle_layer_z_index_updated_event(
 fn handle_layer_deleted_events(
     mut layer_deleted_event_reader: bevy::ecs::event::EventReader<rgis_events::LayerDeletedEvent>,
     mut commands: Commands,
-    query: Query<(&rgis_layer_id::LayerId, Entity), With<Handle<ColorMaterial>>>,
+    query: Query<
+        (&rgis_layer_id::LayerId, Entity),
+        Or<(With<Handle<ColorMaterial>>, With<Handle<Image>>)>,
+    >,
 ) {
     for event in layer_deleted_event_reader.iter() {
         for (_, entity) in query.iter().filter(|(i, _)| **i == event.0) {
