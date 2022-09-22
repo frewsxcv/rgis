d2e0eebc4d03505ea6efefa69a7a8ebb16939978
Corey Farwell <coreyf@rwell.org>
2022-09-22 20:55:01 +0200

    avoid an unnecessary unwrap

--- geo-file-loader/src/wkt.rs
+++ geo-file-loader/src/wkt.rs
@@ -2,12 +2,20 @@ use std::io;
 
 use geozero::GeozeroDatasource;
 
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("{0}")]
+    BoundingRect(#[from] geo_features::BoundingRectError),
+    #[error("{0}")]
+    Geozero(#[from] geozero::error::GeozeroError),
+}
+
 pub struct WktSource {
     pub bytes: Vec<u8>,
 }
 
 impl crate::FileLoader for WktSource {
-    type Error = geozero::error::GeozeroError;
+    type Error = Error;
 
     const FILE_TYPE_NAME: &'static str = "WKT";
 
@@ -21,6 +29,6 @@ impl crate::FileLoader for WktSource {
         let mut geo_writer = geozero::geo_types::GeoWriter::new();
         wkt_reader.process(&mut geo_writer)?;
         let geometry = geo_writer.take_geometry().unwrap();
-        Ok(geo_features::FeatureCollection::from_geometry(geometry).unwrap())
+        Ok(geo_features::FeatureCollection::from_geometry(geometry)?)
     }
 }
