2bb77ee68a042805199b81b8c42cedad13b3e058
frewsxcv <coreyf@rwell.org>
2022-12-16 18:04:03 +0000

    Deploying to gh-pages from @ frewsxcv/rgis@406d865b818f84711fc73327ea24c0e4357f282f 🚀

--- bootstrap.js
+++ bootstrap.js
@@ -1374,7 +1374,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../rgis/pkg/rgis_bg.wasm":"be88bdd2cba156cb6a81"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../rgis/pkg/rgis_bg.wasm":"876ca14107d0f6fb1106"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
