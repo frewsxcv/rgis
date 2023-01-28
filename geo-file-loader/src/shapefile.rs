419981cb652a94714679baa3c2d105de4a3cd1af
Corey Farwell <coreyf@rwell.org>
2023-01-27 23:44:39 -0500

    remove completed todos

--- geo-file-loader/src/shapefile.rs
+++ geo-file-loader/src/shapefile.rs
@@ -17,8 +17,6 @@ impl crate::FileLoader for ShapefileSource {
         let mut bytes_cursor = io::Cursor::new(&self.bytes);
         let shapefile_reader = geozero_shp::Reader::new(&mut bytes_cursor)?;
         let mut geo_writer = geozero::geo_types::GeoWriter::new();
-        // TODO: iter_geometries
-        // TODO: iter_features
         for _ in shapefile_reader.iter_geometries(&mut geo_writer) {}
         let geometry = geo_writer.take_geometry().unwrap();
         Ok(geo_features::FeatureCollection::from_geometry(geometry).unwrap())
