7b4638a07e2d1b8336bdd5c49c6b00e6bde5f12b
Corey Farwell <coreyf@rwell.org>
2023-04-08 22:33:15 -0400

    use geo-bevy fork

--- rgis-renderer/src/jobs.rs
+++ rgis-renderer/src/jobs.rs
@@ -1,3 +1,5 @@
+use geo_bevy::BuildBevyMeshes;
+
 pub struct MeshBuildingJob {
     pub layer_id: rgis_layer_id::LayerId,
     pub color: bevy::render::color::Color,
@@ -22,12 +24,7 @@ impl bevy_jobs::Job for MeshBuildingJob {
     fn perform(self, _: bevy_jobs::Context) -> bevy_jobs::AsyncReturn<Self::Outcome> {
         Box::pin(async move {
             Ok(MeshBuildingJobOutcome {
-                prepared_meshes: geo_bevy::build_bevy_meshes(
-                    self.geometry.as_raw(),
-                    self.color,
-                    geo_bevy::BuildBevyMeshesContext::new(),
-                )?
-                .collect::<Vec<_>>(),
+                prepared_meshes: self.geometry.as_raw().build_bevy_meshes(self.color)?,
                 layer_id: self.layer_id,
                 is_selected: self.is_selected,
             })
