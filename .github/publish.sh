5a779a9f61ff19fbb942fa1c765ba6786ab9fe93
Corey Farwell <coreyf@rwell.org>
2023-10-29 14:06:54 -0400

    dont generate typescript bindings

--- .github/publish.sh
+++ .github/publish.sh
@@ -2,5 +2,5 @@
 
 rm -rf www/dist/
 # https://github.com/bevyengine/bevy/issues/9188
-RUSTFLAGS=--cfg=web_sys_unstable_apis wasm-pack build --release rgis
+RUSTFLAGS=--cfg=web_sys_unstable_apis wasm-pack build --no-typescript --release rgis
 (cd www && npm run build)
