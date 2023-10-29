db8ae39d5562726604217a07890c1c5c94d8c80d
Corey Farwell <coreyf@rwell.org>
2023-10-29 13:09:39 -0400

    init window trait

--- rgis-ui/src/lib.rs
+++ rgis-ui/src/lib.rs
@@ -7,6 +7,7 @@
 )]
 
 use bevy::prelude::*;
+use bevy_egui::egui;
 use std::{collections, marker};
 
 mod add_layer_window;
@@ -21,6 +22,103 @@ mod side_panel;
 mod systems;
 mod top_panel;
 mod widgets;
+
+trait Window: egui::Widget {
+    // type State: Resource;
+
+    fn is_visible(&self) -> bool;
+    fn set_visible(&mut self, visible: bool);
+    // fn state(&self) -> &Self::State;
+    // fn state_mut(&mut self) -> &mut Self::State;
+}
+
+struct DebugWindow {
+    state: DebugStatsWindowState,
+}
+
+const FPS_MAX: f64 = 100.;
+
+impl egui::Widget for DebugWindow {
+    fn ui(self, ui: &mut egui::Ui) -> egui::Response {
+        if self.state.history.is_empty() || self.state.timer.tick(time.delta()).just_finished() {
+            let fps = diagnostics
+                .get(FrameTimeDiagnosticsPlugin::FPS)
+                .and_then(|d| d.measurement())
+                .map(|m| m.value);
+            let frame_time = diagnostics
+                .get(FrameTimeDiagnosticsPlugin::FRAME_TIME)
+                .and_then(|d| d.measurement())
+                .map(|m| m.value);
+            let frame_count = diagnostics
+                .get(FrameTimeDiagnosticsPlugin::FRAME_COUNT)
+                .and_then(|d| d.measurement())
+                .map(|m| m.value);
+
+            if let Some(fps) = fps {
+                self.state.history.push_back(fps);
+                last.fps = fps;
+            }
+            if let Some(frame_time) = frame_time {
+                last.frame_time = frame_time;
+            }
+            if let Some(frame_count) = frame_count {
+                last.frame_count = frame_count;
+            }
+
+            if self.state.history.len() >= crate::DEBUG_STATS_HISTORY_LEN {
+                let _ = self.state.history.pop_front();
+            }
+        }
+
+        let sin = if self.state.is_visible {
+            self.state
+                .history
+                .iter()
+                .enumerate()
+                .map(|(x, y)| egui_plot::PlotPoint::new(x as f64, y.min(FPS_MAX)))
+                .collect::<Vec<_>>()
+        } else {
+            vec![]
+        };
+
+        egui::Window::new("Debug")
+            .default_width(200.)
+            .open(&mut self.state.is_visible)
+            .show(ui.ctx(), move |ui| {
+                DebugTable { last: &last }.ui(ui);
+
+                use egui_plot::{Line, Plot, PlotPoints};
+                let line = Line::new(PlotPoints::Owned(sin));
+                Plot::new("fps_plot")
+                    .allow_drag(false)
+                    .allow_boxed_zoom(false)
+                    .allow_scroll(false)
+                    .allow_zoom(false)
+                    .set_margin_fraction((0., 0.).into())
+                    .show_x(false)
+                    .x_axis_formatter(|_, _, _| "".into())
+                    .y_axis_formatter(|n, _, _| format!("{n:?}"))
+                    .include_x(0.)
+                    .include_x(crate::DEBUG_STATS_HISTORY_LEN as f64)
+                    .include_y(0.)
+                    .include_y(FPS_MAX)
+                    .view_aspect(2.) // Width is twice as big as height
+                    .show(ui, |plot_ui| plot_ui.line(line));
+            })
+    }
+}
+
+impl Window for DebugWindow {
+    // type State = DebugStatsWindowState;
+
+    fn is_visible(&self) -> bool {
+        self.state.is_visible
+    }
+
+    fn set_visible(&mut self, visible: bool) {
+        self.state.is_visible = visible;
+    }
+}
 
 pub struct Plugin;
 
