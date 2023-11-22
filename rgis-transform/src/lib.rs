644fd82cbc847db46d21792e75123db62cc97922
Corey Farwell <coreyf@rwell.org>
2023-11-22 15:30:21 -0500

    Migrate from proj4rs to geodesy (#117)
    * Migrate from proj4rs to geodesy
    
    * make it actually work

--- rgis-transform/src/lib.rs
+++ rgis-transform/src/lib.rs
@@ -8,7 +8,8 @@
 
 use geo::{Coord, MapCoords};
 
-pub use proj4rs::errors::Error as TransformError;
+use geodesy::Context;
+pub use geodesy::Error as TransformError;
 
 mod jobs;
 mod systems;
@@ -22,28 +23,60 @@ impl bevy::app::Plugin for Plugin {
 }
 
 pub struct ProjTransformer {
-    source: proj4rs::Proj,
-    target: proj4rs::Proj,
+    ctx: geodesy::Minimal,
+    source: geodesy::OpHandle,
+    target: geodesy::OpHandle,
+}
+
+mod noop {
+    pub const GAMUT: [geodesy::OpParameter; 0] = [];
+
+    pub fn new(
+        parameters: &geodesy::RawParameters,
+        _ctx: &dyn geodesy::Context,
+    ) -> Result<geodesy::Op, geodesy::Error> {
+        geodesy::Op::plain(
+            parameters,
+            geodesy::InnerOp::default(),
+            Some(geodesy::InnerOp::default()),
+            &GAMUT,
+            _ctx,
+        )
+    }
 }
 
 impl ProjTransformer {
-    pub fn setup(source_crs: u16, target_crs: u16) -> proj4rs::errors::Result<Self> {
+    pub fn setup(source_crs: u16, target_crs: u16) -> Result<Self, geodesy::Error> {
+        let source = crs_definitions::from_code(source_crs).unwrap();
+        let target = crs_definitions::from_code(target_crs).unwrap();
+        let mut ctx = geodesy::Minimal::new();
+        // https://github.com/Rennzie/geodesy-wasm/blob/82f9ef050372d53144969dba60807c7dac38c910/src/geodesy/operators/mod.rs#L7-L19
+        //
+        // https://github.com/busstoptaktik/geodesy/issues/79
+        ctx.register_op("longlat", geodesy::OpConstructor(noop::new));
+        let source_geodesy_string = geodesy::parse_proj(source.proj4)?;
+        let source_op_handle = ctx.op(&source_geodesy_string)?;
+        let target_geodesy_string = geodesy::parse_proj(target.proj4)?;
+        let target_op_handle = ctx.op(&target_geodesy_string)?;
         Ok(ProjTransformer {
-            source: proj4rs::Proj::from_epsg_code(source_crs)?,
-            target: proj4rs::Proj::from_epsg_code(target_crs)?,
+            ctx,
+            source: source_op_handle,
+            target: target_op_handle,
         })
     }
 
-    pub fn transform(&self, geometry: &mut geo::Geometry) -> proj4rs::errors::Result<()> {
+    pub fn transform(&self, geometry: &mut geo::Geometry) -> Result<(), geodesy::Error> {
         // FIXME: use try_map_coords_in_place
-        let mut transformed = geometry.try_map_coords::<proj4rs::errors::Error>(|mut coord| {
-            if self.source.is_latlong() {
-                coord.x = coord.x.to_radians();
-                coord.y = coord.y.to_radians();
-            }
-            let (x, y) =
-                proj4rs::adaptors::transform_xy(&self.source, &self.target, coord.x, coord.y)?;
-            Ok(Coord { x, y })
+        let mut transformed = geometry.try_map_coords::<geodesy::Error>(|coord| {
+            let mut coord = [geodesy::Coor2D::gis(coord.x, coord.y)];
+            self.ctx
+                .apply(self.source, geodesy::Direction::Inv, &mut coord)?;
+            self.ctx
+                .apply(self.target, geodesy::Direction::Fwd, &mut coord)?;
+            Ok(Coord {
+                x: coord[0].0[0],
+                y: coord[0].0[1],
+            })
         })?;
 
         std::mem::swap(&mut transformed, geometry);
