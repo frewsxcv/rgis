252267578929a8d1586ecbd2b7ea7195580042b7
Corey Farwell <coreyf@rwell.org>
2023-11-23 14:43:00 -0500

    refactor request fetching

--- rgis-network/src/lib.rs
+++ rgis-network/src/lib.rs
@@ -48,37 +48,10 @@ impl bevy_jobs::Job for NetworkFetchJob {
 
     fn perform(self, ctx: bevy_jobs::Context) -> bevy_jobs::AsyncReturn<Self::Outcome> {
         Box::pin(async move {
-            let fetch = async {
-                let response = reqwest::get(self.url).await?;
-                let total_size = response.content_length().unwrap_or(0);
-                let mut bytes_stream = response.bytes_stream();
-                let mut bytes = Vec::<u8>::with_capacity(total_size as usize);
-
-                while let Some(bytes_chunk) = bytes_stream.next().await {
-                    let mut bytes_chunk = Vec::from(bytes_chunk?);
-                    bytes.append(&mut bytes_chunk);
-                    if total_size > 0 {
-                        let percent = 100 * bytes.len() / total_size as usize;
-                        let _ = ctx
-                            .send_progress(percent as u8)
-                            .await;
-                    }
-                }
-
-                Ok(FetchedFile {
-                    bytes: bytes::Bytes::from(bytes),
-                    crs_epsg_code: self.crs_epsg_code,
-                    name: self.name,
-                })
-            };
-            #[cfg(not(target_arch = "wasm32"))]
-            {
-                TOKIO_RUNTIME.block_on(fetch)
-            }
-            #[cfg(target_arch = "wasm32")]
-            {
-                fetch.await
-            }
+            await_future(async move {
+                build_request_future(self.url, self.crs_epsg_code, self.name, ctx).await
+            })
+            .await
         })
     }
 }
@@ -87,4 +60,44 @@ pub struct Plugin;
 
 impl bevy::app::Plugin for Plugin {
     fn build(&self, _app: &mut bevy::app::App) {}
+}
+
+async fn build_request_future(
+    url: String,
+    crs_epsg_code: u16,
+    name: String,
+    ctx: bevy_jobs::Context,
+) -> Result<FetchedFile, Error> {
+    let response = reqwest::get(url).await?;
+    let total_size = response.content_length().unwrap_or(0);
+    let mut bytes_stream = response.bytes_stream();
+    let mut bytes = Vec::<u8>::with_capacity(total_size as usize);
+
+    while let Some(bytes_chunk) = bytes_stream.next().await {
+        let mut bytes_chunk = Vec::from(bytes_chunk?);
+        bytes.append(&mut bytes_chunk);
+        if total_size > 0 {
+            let percent = 100 * bytes.len() / total_size as usize;
+            let _ = ctx.send_progress(percent as u8).await;
+        }
+    }
+
+    Ok(FetchedFile {
+        bytes: bytes::Bytes::from(bytes),
+        crs_epsg_code,
+        name,
+    })
+}
+
+async fn await_future<Output>(
+    future: impl std::future::Future<Output = Output> + 'static,
+) -> Output {
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        TOKIO_RUNTIME.block_on(future)
+    }
+    #[cfg(target_arch = "wasm32")]
+    {
+        fetch.await
+    }
 }
