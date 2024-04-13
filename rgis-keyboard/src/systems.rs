010c1185d9a9c58ac2ade8e02a3272767c2e71ce
Corey Farwell <coreyf@rwell.org>
2024-04-13 18:43:06 -0400

    remove commented out controls

--- rgis-keyboard/src/systems.rs
+++ rgis-keyboard/src/systems.rs
@@ -5,7 +5,6 @@ const PAN_AMOUNT: f32 = 15.; // Larger number will pan more
 fn process_key_code_input_system(
     keyboard_input: Res<bevy::input::ButtonInput<bevy::input::keyboard::KeyCode>>,
     mut pan_camera_events: bevy::ecs::event::EventWriter<rgis_events::PanCameraEvent>,
-    // mut zoom_camera_events: bevy::ecs::event::EventWriter<rgis_events::ZoomCameraEvent>,
 ) {
     for key in keyboard_input.get_just_pressed() {
         match key {
@@ -21,16 +20,6 @@ fn process_key_code_input_system(
             bevy::input::keyboard::KeyCode::ArrowLeft => {
                 pan_camera_events.send(rgis_events::PanCameraEvent::left(PAN_AMOUNT));
             }
-            // bevy::input::keyboard::KeyCode::Minus => {
-            //     zoom_camera_events.send(rgis_events::ZoomCameraEvent::zoom_out())
-            // }
-            // bevy::input::keyboard::KeyCode::Equals => {
-            //     if keyboard_input.pressed(bevy::input::keyboard::KeyCode::RShift)
-            //         || keyboard_input.pressed(bevy::input::keyboard::KeyCode::LShift)
-            //     {
-            //         zoom_camera_events.send(rgis_events::ZoomCameraEvent::zoom_in())
-            //     }
-            // }
             _ => {}
         }
     }
