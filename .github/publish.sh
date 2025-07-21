7a8d059eaf11612f40163917c1edf84303841674
Corey Farwell <coreyf@rwell.org>
2025-07-21 15:11:33 -0400

    attempt to remove wasm-pack

--- .github/publish.sh
+++ .github/publish.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -ex
 
 rm -rf www/dist/
+rm -rf rgis/pkg
 # https://github.com/bevyengine/bevy/issues/9188
-RUSTFLAGS=--cfg=web_sys_unstable_apis wasm-pack build --no-typescript --release rgis
+RUSTFLAGS=--cfg=web_sys_unstable_apis cargo build --release --target wasm32-unknown-unknown -p rgis
+wasm-bindgen --out-dir rgis/pkg --target web --no-typescript "target/wasm32-unknown-unknown/release/rgis.wasm"
 (cd www && npm run build)
