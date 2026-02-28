b897ef631bd3f6e8c5c8ee5acbb850e6ba0e7c05
Corey Farwell <coreyf@rwell.org>
2026-02-28 04:42:28 -0500

    use nightly + -Zinline-mir=no for wasm32 build
    Workaround for https://github.com/rust-lang/rust/issues/131960
    which causes E0275 recursion overflow in release builds.

--- .github/publish.sh
+++ .github/publish.sh
@@ -3,6 +3,7 @@
 rm -rf www/dist/
 rm -rf rgis/pkg
 # https://github.com/bevyengine/bevy/issues/9188
-RUSTFLAGS=--cfg=web_sys_unstable_apis cargo build --release --target wasm32-unknown-unknown -p rgis
+# -Zinline-mir=no: workaround for https://github.com/rust-lang/rust/issues/131960
+RUSTFLAGS="$RUSTFLAGS --cfg=web_sys_unstable_apis -Zinline-mir=no" cargo build --release --target wasm32-unknown-unknown -p rgis
 wasm-bindgen --out-dir rgis/pkg --target web --no-typescript "target/wasm32-unknown-unknown/release/rgis.wasm"
 (cd www && npm run build)
