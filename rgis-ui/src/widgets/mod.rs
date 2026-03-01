c806b2147d2a0b4713cbb4ddff38692d8022f1cb
Corey Farwell <coreyf@rwell.org>
2026-03-01 16:23:20 -0500

    Remove unused toggle_layer and move_up_move_down widgets
    Their functionality was moved inline to the layer context menu.

--- rgis-ui/src/widgets/mod.rs
+++ rgis-ui/src/widgets/mod.rs
@@ -2,9 +2,7 @@ pub mod add_layer;
 pub mod crs_input;
 pub mod debug_table;
 pub mod feature_properties_table;
-pub mod move_up_move_down;
 pub mod operations;
-pub mod toggle_layer;
 
 pub mod fill_color;
 pub mod point_size;
