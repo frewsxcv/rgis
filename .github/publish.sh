e4c01a92a326320bcb64b18ef122f6e38b37a5a0
Corey Farwell <coreyf@rwell.org>
2026-03-03 13:19:56 -0500

    Fix wasm32 build by adding --lib flag to cargo build
    The rgis crate has both bin and lib (cdylib) targets that produce
    the same output filename (rgis.wasm). Without --lib, the bin target
    overwrites the cdylib, causing wasm-bindgen to panic on invalid input.

--- .github/publish.sh
+++ .github/publish.sh
@@ -4,6 +4,6 @@ rm -rf www/dist/
 rm -rf rgis/pkg
 # https://github.com/bevyengine/bevy/issues/9188
 # -Zinline-mir=no: workaround for https://github.com/rust-lang/rust/issues/131960
-RUSTFLAGS="$RUSTFLAGS --cfg=web_sys_unstable_apis -Zinline-mir=no" cargo build --release --target wasm32-unknown-unknown -p rgis
+RUSTFLAGS="$RUSTFLAGS --cfg=web_sys_unstable_apis -Zinline-mir=no" cargo build --release --target wasm32-unknown-unknown -p rgis --lib
 wasm-bindgen --out-dir rgis/pkg --target web --no-typescript "target/wasm32-unknown-unknown/release/rgis.wasm"
 (cd www && npm run build)
